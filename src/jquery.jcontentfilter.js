05cc5bed541f799531c638a880d6031dcc566d5c
Billy Monk <billy@zoocasa.com>
2012-07-10 11:19:24 -0400

    Add initial implementation of jContentFilter.
    A basic implementation that requires a selector for elements used as filters
    as well as a selector for elements that will be filtered.
    Optionally allows for before/after callbacks.
    Assumes the class used for resetting the filtered content back to its original state.

---
+++ src/jquery.jcontentfilter.js
@@ -0,0 +1,47 @@
+; (function($) {
+  "use strict";
+
+  $.jContentFilter = {};
+
+  $.jContentFilter.options = {
+    "filterSelector": "",
+    "contentSelector": "",
+    "reset": "reset",
+    "beforeFilter": null,
+    "afterFilter": null
+  };
+
+  function _filterContent(element) {
+    var $this = $(element),
+        $contentSelector = $($.jContentFilter.options.contentSelector),
+        $that = $this;
+
+    if ($.jContentFilter.options.beforeFilter) {
+      $.jContentFilter.options.beforeFilter.call(element);
+    }
+
+    $contentSelector.each(function(index) {
+      var $this = $(this);
+
+      if ($.inArray($that.attr("class"), $this.attr("class").split(" ")) < 0 && $that.attr("class") !== $.jContentFilter.options.reset) {
+        $this.hide();
+      } else {
+        $this.show();
+      }
+    });
+
+    if ($.jContentFilter.options.afterFilter) {
+      $.jContentFilter.options.afterFilter.call(element);
+    }
+  }
+
+  $.jContentFilter.filter = function(options) {
+    $.extend($.jContentFilter.options, options);
+
+    $($.jContentFilter.options.filterSelector).each(function(index) {
+      var $this = $(this);
+
+      $this.bind("click.jContentFilter", function(event) { _filterContent(event.target); });
+    });
+  };
+}(jQuery));
